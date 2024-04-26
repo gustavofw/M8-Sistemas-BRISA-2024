39dac945ce2398500a2b2f7731559661def7f8db
Gabriel-Woicie <144628124+Gabriel-Woicie@users.noreply.github.com>
2024-04-25 21:12:15 -0300

    Add files via upload

--- backend/main.py
+++ backend/main.py
@@ -1,25 +1,26 @@
 import spacy
 
+
 nlp = spacy.load("pt_core_news_lg")
-with open ("./Artigo_Compras.txt") as f:
-    text = f.read()
-user = input("Qual a sua dúvida? ")
-tokens = ["compras", "compra", "Compras", "Compra"]
-for token in tokens:
-    if token in user:
-        doc = nlp(text)
-        print(len(text))
-        print(len(doc))
-        for token in text[0:10]:
-            print(token)
-        for token in doc[:10]:
-            print(token)
-        for token in text.split()[:10]:
-            print(token)
-        for sent in doc.sents:
-            print(sent)
-        sentence1 = list(doc.sents)[2]
-        print(sentence1)
+
+def process_user_input(input_text):
+    doc = nlp(input_text.lower())  # Convertendo a entrada para minúsculas
+    
+    compra_achou = any(token.lemma_ == "comprar" for token in doc) # Verifica se contém "compra" em qualquer forma 
+    export_nfe_palavraschaves = ["exportação", "nfe", "nota", "fiscal", "eletrônica"] # Verifica se contém as palavras-chave relacionadas exportação NFe
+    export_nfe_achou = any(word in input_text.lower() for word in export_nfe_palavraschaves) 
+
+    if compra_achou and not export_nfe_achou:
+        print("Você mencionou compra. Talvez queira saber mais sobre a exportação de NFe? Posso ajudar com isso!")
+    elif export_nfe_achou:
+        print("Aqui está um link para ajudar com a exportação da NFe: https://www.nfe.fazenda.gov.br/portal/principal.aspx")
+    else:
+        print("Não identifiquei sua dúvida sobre compras ou NFe. Pode especificar mais?")
+
+# Loop para manter o chatbot rodando
+while True:
+    user_input = input("Qual a sua dúvida? ")
+    if user_input.lower() == 'sair':
+        print("Obrigado por usar o chatbot. Até mais!")
         break
-else:
-    print("Nenhuma das palavras-chave foi encontrada na frase.") 
+    process_user_input(user_input)
